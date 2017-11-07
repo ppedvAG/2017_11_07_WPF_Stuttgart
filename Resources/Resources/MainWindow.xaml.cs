e9a919ee647c1d2fa33e8edc0c17e3e8f4b9a263
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-11-07 17:08:16 +0100

    Resources

---
+++ Resources/Resources/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace Resources
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void changeColor(object sender, RoutedEventArgs e)
+        {
+            var brush = Resources["defaultBackgroundBrush"] as SolidColorBrush;
+            if (brush != null)
+                brush.Color = Colors.Gold;
+        }
+
+        private void changeBrush(object sender, RoutedEventArgs e)
+        {
+            Resources["defaultBackgroundBrush"] = new SolidColorBrush(Colors.Fuchsia);
+        }
+
+        private void changeBrushType(object sender, RoutedEventArgs e)
+        {
+            Resources["defaultBackgroundBrush"] = new RadialGradientBrush(Colors.GhostWhite, Colors.DarkCyan);
+        }
+    }
+}
