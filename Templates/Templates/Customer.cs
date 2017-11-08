655d3482116e644a9e633884b3ed29e5eaa7191e
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-11-08 11:03:16 +0100

    Data Template

---
+++ Templates/Templates/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace Templates
+{
+    internal class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsPrime { get; set; }
+    }
+}
--- Templates/Templates/MainWindow.xaml.cs
+++ Templates/Templates/MainWindow.xaml.cs
@@ -1,28 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Templates
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public MainWindow() => InitializeComponent();
+
+        private void Button_Click(object sender, RoutedEventArgs e) => MessageBox.Show("Was clicked!");
+
+        private void CreateCustomer(object sender, RoutedEventArgs e) => customerControl.Content = new Customer
         {
-            InitializeComponent();
-        }
+            Id = 39456,
+            Name = "Franz Xaver Gruber",
+            IsPrime = true
+        };
     }
 }
