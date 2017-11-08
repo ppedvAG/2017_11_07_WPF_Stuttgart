07c1e0c3b38adc363f100bcb9321c7b0f6eeff1c
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-11-08 14:50:51 +0100

    Converter

---
+++ ConvertersExample/ConvertersExample/Converters/BoolToBrushConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ConvertersExample.Converters
+{
+    internal class BoolToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => (bool)value ? Brushes.Green : Brushes.Red;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
