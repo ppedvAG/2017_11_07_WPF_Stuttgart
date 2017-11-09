01ee2f92cb6cb9776084c2bf16c453068d96b127
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-11-09 14:21:24 +0100

    ValidationRules & Error Template

---
+++ Validation/Validation/MustNotBe9ValidationRule.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace Validation
+{
+    internal class MustNotBe9ValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo) => 
+            value.Equals("9")
+             ? new ValidationResult(false, "Must not be 9! :(")
+             : ValidationResult.ValidResult;
+    }
+}
