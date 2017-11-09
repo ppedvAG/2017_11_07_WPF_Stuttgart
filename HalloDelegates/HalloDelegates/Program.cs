729ba4a15bc3f4a65d40721e3b28b4f56d0cde19
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-11-09 14:35:01 +0100

    HalloDelegates

---
+++ HalloDelegates/HalloDelegates/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace HalloDelegates
+{
+    internal delegate string MyDelegate(int i, double d);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate del = new MyDelegate(MeineWichtigeMethode);
+
+            var result = del.Invoke(6, 8.0);
+
+            System.Console.WriteLine(result);
+        }
+
+        private static string MeineWichtigeMethode(int zahl, double wert) => (zahl + wert).ToString();
+    }
+}
