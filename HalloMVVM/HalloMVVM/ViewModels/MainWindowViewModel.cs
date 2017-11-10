1ddb1b76615a5bf053ea9208cf294d4536bcc10e
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-11-10 11:09:27 +0100

    MVVM

---
+++ HalloMVVM/HalloMVVM/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HalloMVVM.ViewModels
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        private string welcomeText = "Hallo MVVM";
+        public string WelcomeText
+        {
+            get => welcomeText;
+            set => Set(ref welcomeText, value);
+        }
+
+        public Command ChangeTextCommand { get; }
+            
+        public MainWindowViewModel() =>
+            ChangeTextCommand = new Command(
+                () => WelcomeText = "Mein Text aus dem VM",
+                () => WelcomeText.Length < 10);
+    }
+}
---
+++ HalloMVVM/HalloMVVM/ViewModels/ViewModelBase.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace HalloMVVM.ViewModels
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        protected void Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (field == null || !field.Equals(value))
+            {
+                field = value;
+                RaisePropertyChanged(propertyName);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
