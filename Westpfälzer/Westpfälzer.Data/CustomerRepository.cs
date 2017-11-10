c75af44dab8a58a4291688a7ece388d69235162d
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-11-10 12:09:59 +0100

    Create Data

---
+++ Westpfälzer/Westpfälzer.Data/CustomerRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tynamix.ObjectFiller;
+using Westpfälzer.Core;
+using Westpfälzer.Core.Models;
+
+namespace Westpfälzer.Data
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly IEnumerable<Customer> customers;
+
+        public CustomerRepository()
+        {
+            var addressFiller = new Filler<Address>();
+            var addressSetup = addressFiller.Setup()
+                .OnProperty(a => a.City).Use(new CityName())
+                .OnProperty(a => a.Country).Use(new CountryName())
+                .OnProperty(a => a.Street).Use(new StreetName())
+                .OnProperty(a => a.ZipCode).Use(new PatternGenerator("{C:10000}"))
+                .Result;
+
+            var customerFiller = new Filler<Customer>();
+            customerFiller.Setup()
+                .OnProperty(c => c.Address).Use(addressSetup)
+                .OnProperty(c => c.Firstname).Use(new RealNames(NameStyle.FirstName))
+                .OnProperty(c => c.Lastname).Use(new RealNames(NameStyle.LastName))
+                .OnProperty(c => c.Email).Use(new EmailAddresses(".de"));
+
+            var random = new Random();
+            customers = customerFiller.Create(random.Next(50, 500));
+        }
+
+        public Task<IEnumerable<Customer>> GetAll() => Task.FromResult(customers);
+    }
+}
