2b99c218f2622775a39f69cf971f8a912ac3deaa
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-11-10 14:49:29 +0100

    Implement CustomersViewModel

--- Westpfälzer/Westpfälzer.Ui/ViewModels/CustomersViewModel.cs
+++ Westpfälzer/Westpfälzer.Ui/ViewModels/CustomersViewModel.cs
@@ -1,6 +1,37 @@
-﻿namespace Westpfälzer.Ui.ViewModels
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Westpfälzer.Core;
+using Westpfälzer.Core.Models;
+
+namespace Westpfälzer.Ui.ViewModels
 {
     internal class CustomersViewModel : ViewModelBase
     {
+        private readonly ICustomerRepository customerRepository;
+
+        private IEnumerable<Customer> customers;
+        public IEnumerable<Customer> Customers
+        {
+            get => customers;
+            set
+            {
+                Set(ref customers, value);
+                RaisePropertyChanged(nameof(CountCustomers));
+            }
+        }
+
+        public int? CountCustomers => Customers?.Count();
+
+        public Command LoadCustomersCommand { get; }
+
+        public CustomersViewModel(ICustomerRepository customerRepository)
+        {
+            this.customerRepository = customerRepository ?? throw new ArgumentNullException(nameof(customerRepository));
+
+            LoadCustomersCommand = new Command(
+                async () => Customers = (await customerRepository.GetAll()).OrderBy(c => c.Firstname),
+                () => Customers is null);
+        }
     }
 }
--- Westpfälzer/Westpfälzer.Ui/ViewModels/ViewModelLocator.cs
+++ Westpfälzer/Westpfälzer.Ui/ViewModels/ViewModelLocator.cs
@@ -1,14 +1,22 @@
-﻿namespace Westpfälzer.Ui.ViewModels
+﻿using StructureMap;
+using Westpfälzer.Core;
+using Westpfälzer.Data;
+
+namespace Westpfälzer.Ui.ViewModels
 {
     internal class ViewModelLocator
     {
-        public CustomersViewModel CustomersViewModel { get; }
-        public MainWindowViewModel MainWindowViewModel { get; }
+        private readonly IContainer container;
+
+        public CustomersViewModel CustomersViewModel => container.GetInstance<CustomersViewModel>();
+        public MainWindowViewModel MainWindowViewModel => container.GetInstance<MainWindowViewModel>();
 
         public ViewModelLocator()
         {
-            CustomersViewModel = new CustomersViewModel();
-            MainWindowViewModel = new MainWindowViewModel();
+            container = new Container(c =>
+            {
+                c.For<ICustomerRepository>().Use<CustomerRepository>();
+            });
         }
     }
 }
